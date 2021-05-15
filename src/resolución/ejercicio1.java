32db15470ee93b65f1677831a9ea694f331a929b
andres.chapo <andres.chapo@bue.edu.ar>
2021-05-15 13:15:50 -0300

    Ejercicio1

---
+++ src/resolución/ejercicio1.java
@@ -0,0 +1,14 @@
+package resolución;
+
+//Crear un main llamado ejercicio1. Dentro de este main crear 5 entradas
+//y guardarlas dentro de un ArrayList. Mostrar las entradas guardadas en
+//el ArrayList con un iterador.
+
+public class ejercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ejercicio1");
+	}
+
+}
