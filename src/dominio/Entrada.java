0b2f979efdb1ee028869adf05ee9c16bbb739eb3
andres.chapo <andres.chapo@bue.edu.ar>
2021-05-15 21:32:39 -0300

    Ejercicio 1 y 2 completados

--- src/dominio/Entrada.java
+++ src/dominio/Entrada.java
@@ -3,7 +3,7 @@ package dominio;
 import java.sql.Time;
 import java.sql.Timestamp;
 
-public class Entrada {
+public class Entrada implements Comparable<Entrada>{
 	private int idEntrada;
 	private int idEvento;
 	private double precio;
@@ -64,5 +64,48 @@ public class Entrada {
 		return "Entrada [idEntrada=" + idEntrada + ", idEvento=" + idEvento + ", precio="
 				+ precio + ", categoria=" + categoria + "]";
 	}
+
+	@Override
+	public int compareTo(Entrada o) {
+
+		return idEntrada - o.getIdEntrada();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((categoria == null) ? 0 : categoria.hashCode());
+		result = prime * result + idEntrada;
+		result = prime * result + idEvento;
+		long temp;
+		temp = Double.doubleToLongBits(precio);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Entrada other = (Entrada) obj;
+		if (categoria == null) {
+			if (other.categoria != null)
+				return false;
+		} else if (!categoria.equals(other.categoria))
+			return false;
+		if (idEntrada != other.idEntrada)
+			return false;
+		if (idEvento != other.idEvento)
+			return false;
+		if (Double.doubleToLongBits(precio) != Double.doubleToLongBits(other.precio))
+			return false;
+		return true;
+	}
+
 }
 //vip/gral/edad - recital
--- src/resolución/ejercicio1.java
+++ src/resolución/ejercicio1.java
@@ -1,5 +1,15 @@
 package resolución;
 
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+import dominio.Deportivo;
+import dominio.Entrada;
+import dominio.Evento;
+import dominio.Tipos;
+
 //Crear un main llamado ejercicio1. Dentro de este main crear 5 entradas
 //y guardarlas dentro de un ArrayList. Mostrar las entradas guardadas en
 //el ArrayList con un iterador.
@@ -7,8 +17,30 @@ package resolución;
 public class ejercicio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("ejercicio1");
+		Timestamp ahora = new Timestamp(System.currentTimeMillis());
+		Time duracion = new Time(2, 30, 0);
+		Tipos tipos = new Tipos();
+		
+		Deportivo eve = new Deportivo("Supercampeones", ahora, duracion,tipos.getDeporte(1),true);
+		
+		Entrada ticket1 = new Entrada(eve.getNumero(), eve.getDeporte().getPrecio(), eve.getDeporte().getNombre() +" Internacional");
+		Entrada ticket2 = new Entrada(eve.getNumero(), eve.getDeporte().getPrecio(), eve.getDeporte().getNombre() +" Internacional");
+		Entrada ticket3 = new Entrada(eve.getNumero(), eve.getDeporte().getPrecio(), eve.getDeporte().getNombre() +" Internacional");
+		Entrada ticket4 = new Entrada(eve.getNumero(), eve.getDeporte().getPrecio(), eve.getDeporte().getNombre() +" Internacional");
+		Entrada ticket5 = new Entrada(eve.getNumero(), eve.getDeporte().getPrecio(), eve.getDeporte().getNombre() +" Internacional");
+		
+		ArrayList<Entrada> listaEntradas = new ArrayList<Entrada>(); 
+		listaEntradas.add(ticket1);
+		listaEntradas.add(ticket3);
+		listaEntradas.add(ticket2);
+		listaEntradas.add(ticket5);	
+		listaEntradas.add(ticket4);
+
+		System.out.println("Ejercicio 1 - a:");
+		for (Entrada e: listaEntradas) {
+			System.out.println(e);
+		}
+		
 	}
 
 }
